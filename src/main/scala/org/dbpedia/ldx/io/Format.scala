e88f8853a7d3dc0795d0f0a57c9538b485c8791f
Denis <yum-yab-dev@pm.me>
2022-11-15 17:42:14 +0100

    Bit of refactoring

--- src/main/scala/org/dbpedia/ldx/io/Format.scala
+++ src/main/scala/org/dbpedia/ldx/io/Format.scala
@@ -13,43 +13,41 @@ sealed trait Format {
   val name: String
 }
 
-case class RDF_NTRIPLES() extends Format {
-  override val mediaType: String = "application/n-triples"
-  override val jenaLang: Lang = Lang.NTRIPLES
-  override val name: String = "ntriples"
-}
-
-case class RDF_TURTLE() extends Format {
-  override val mediaType: String = "text/turtle"
-  override val jenaLang: Lang = Lang.TURTLE
-  override val name: String = "turtle"
-}
-
-case class RDF_XML() extends Format {
-  override val mediaType: String = "application/rdf+xml"
-  override val jenaLang: Lang = Lang.RDFXML
-  override val name: String = "rdfxml"
-}
-
-case class RDF_JSONLD() extends Format {
-  override val mediaType: String = "application/ld+json"
-  override val jenaLang: Lang = Lang.JSONLD
-  override val name: String = "jsonld"
-}
-
-case class RDF_NQUADS() extends Format {
-  override val mediaType: String = "application/n-quads"
-  override val jenaLang: Lang = Lang.NQUADS
-  override val name: String = "nquads"
-}
-
-case class RDF_TRIG() extends Format {
-  override val mediaType: String = "application/trig"
-  override val jenaLang: Lang = Lang.TRIG
-  override val name: String = "trig"
-}
-
 object Format {
+  case class RDF_NTRIPLES() extends Format {
+    override val mediaType: String = "application/n-triples"
+    override val jenaLang: Lang = Lang.NTRIPLES
+    override val name: String = "ntriples"
+  }
+
+  case class RDF_TURTLE() extends Format {
+    override val mediaType: String = "text/turtle"
+    override val jenaLang: Lang = Lang.TURTLE
+    override val name: String = "turtle"
+  }
+
+  case class RDF_XML() extends Format {
+    override val mediaType: String = "application/rdf+xml"
+    override val jenaLang: Lang = Lang.RDFXML
+    override val name: String = "rdfxml"
+  }
+
+  case class RDF_JSONLD() extends Format {
+    override val mediaType: String = "application/ld+json"
+    override val jenaLang: Lang = Lang.JSONLD
+    override val name: String = "jsonld"
+  }
+
+  case class RDF_TRIG() extends Format {
+    override val mediaType: String = "application/trig"
+    override val jenaLang: Lang = Lang.TRIG
+    override val name: String = "trig"
+  }
+  case class RDF_NQUADS() extends Format {
+    override val mediaType: String = "application/n-quads"
+    override val jenaLang: Lang = Lang.NQUADS
+    override val name: String = "nquads"
+  }
 
   import scala.reflect.runtime.{universe => ru}
 
